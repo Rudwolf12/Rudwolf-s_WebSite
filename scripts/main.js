16c78e89ad18ad8e90960b22204bc4034357f23a
Rudwolf <rodolfopemc@outlook.com>
2024-06-23 00:22:22 -0600

    Yes

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,13 @@
 console.log("¡Hola mundo desde JavaScript!");
+
+function UnfoldMobileMenu()
+{
+    if (document.querySelector('.menu-row-mobile .menu-dropdown-mobile .menu-dropdown-content-mobile').style.display == 'block')
+    {
+        document.querySelector('.menu-row-mobile .menu-dropdown-mobile .menu-dropdown-content-mobile').style.display = 'none';
+    }
+    else
+    {
+        document.querySelector('.menu-row-mobile .menu-dropdown-mobile .menu-dropdown-content-mobile').style.display = 'block';
+    }
+}
